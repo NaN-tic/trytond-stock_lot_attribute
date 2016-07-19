93de65a9dfbce370b4502530f5b96496bcf0a01b
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-19 11:20:19 +0200

    Update test to version 4.0

--- tests/test_stock_lot_attribute.py
+++ tests/test_stock_lot_attribute.py
@@ -1,33 +1,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-# import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-# TODO: Remove if no sceneario needed.
-# from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_lot_attribute')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_lot_attribute')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_lot_attribute'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_stock_lot_attribute.rst',
-    #        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
